480d787030aa8d32b92f28d30bd2a342602484fb
Rakesh Roshan <rakeshraimca@gmail.com>
2019-01-04 00:09:22 +0530

    bringing back libtinycompress

--- msm8953-common/Android.mk
+++ msm8953-common/Android.mk
@@ -135,16 +135,16 @@ include $(BUILD_PREBUILT)
 
 
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := libtinycompress_vendor
-#LOCAL_MODULE_OWNER := xiaomi
-#LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libtinycompress_vendor.so
-#LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libtinycompress_vendor.so
-#LOCAL_MULTILIB := both
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-#LOCAL_MODULE_SUFFIX := .so
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtinycompress_vendor
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libtinycompress_vendor.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libtinycompress_vendor.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libgpustats
